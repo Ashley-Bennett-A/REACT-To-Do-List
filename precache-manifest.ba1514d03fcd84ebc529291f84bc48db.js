6d8b32ed83efaf8720c825639420189b4d6391e2
Ashley Benntt <ashleyben21@gmail.com>
2019-06-24 11:27:04 +0100

    Updates

---
+++ precache-manifest.ba1514d03fcd84ebc529291f84bc48db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0757af9d1648c7dedd4392e321fd47d",
+    "url": "/REACT-To-Do-List/index.html"
+  },
+  {
+    "revision": "3566d84aea4f5c8b43c8",
+    "url": "/REACT-To-Do-List/static/css/main.f5ebba1f.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/REACT-To-Do-List/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "3566d84aea4f5c8b43c8",
+    "url": "/REACT-To-Do-List/static/js/main.33b40c8f.chunk.js"
+  },
+  {
+    "revision": "83aa4cc6826589c20989",
+    "url": "/REACT-To-Do-List/static/js/runtime~main.b42d224b.js"
+  }
+]);
